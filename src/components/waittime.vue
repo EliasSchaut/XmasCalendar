3f9e3fbda3d54ce2390bc91856a17b865c1bb278
EliasSchaut <eschaut@outlook.de>
2023-11-18 20:09:46 +0100

    improve waittime display to include days

--- src/components/waittime.vue
+++ src/components/waittime.vue
@@ -4,13 +4,22 @@
 let time_left = ref('unknown');
 useFetch('/api/task/waittime').then((data) => {
   if (data.data.value === null) return;
+  if (data.data.value <= 60) {
+    time_left.value = 'under a minute!';
+    return;
+  }
 
-  const hours_left = Math.floor(data.data.value / (60 * 60));
-  const minutes_left = Math.floor(
-    (data.data.value - hours_left * 60 * 60) / 60,
+  const days_left = Math.floor(data.data.value / (60 * 60 * 24));
+  const hours_left = Math.floor(
+    (data.data.value - days_left * 60 * 60 * 24) / (60 * 60),
   );
-  time_left.value = `${
-    hours_left > 0 ? `${hours_left} hours` : ``
-  } ${minutes_left} minutes`;
+  const minutes_left = Math.floor(
+    (data.data.value - days_left * 60 * 60 * 24 - hours_left * 60 * 60) / 60,
+  );
+
+  time_left.value = `
+    ${days_left > 0 ? `${days_left} days` : ``}
+    ${hours_left > 0 ? `${hours_left} hours` : ``}
+    ${minutes_left > 0 ? `${minutes_left} minutes` : ``}!`;
 });
 </script>
--- src/server/api/task/waittime.ts
+++ src/server/api/task/waittime.ts
@@ -2,6 +2,10 @@ import tasks from '~~/tasks';
 
 export default defineEventHandler(() => {
   const current_timestamp = Math.floor(Date.now() / 1000);
+  if (current_timestamp < tasks.start_unix_timestamp) {
+    return tasks.start_unix_timestamp - current_timestamp;
+  }
+
   const index =
     Math.floor((current_timestamp - tasks.start_unix_timestamp) / 86400) + 1;
   return tasks.start_unix_timestamp + index * 86400 - current_timestamp;
