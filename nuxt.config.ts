dbc595c45b2bba2c6d99daabf5a1e9247b52b3a5
EliasSchaut <eschaut@outlook.de>
2023-11-18 16:17:19 +0100

    init nuxt

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxtjs/color-mode', '@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: 'CardDraw'
+    }
+  },
+  srcDir: 'src/',
+  devtools: { enabled: true },
+  tailwindcss: {
+    configPath: 'tailwind.config.ts',
+  },
+  colorMode: {
+    preference: 'system',
+    fallback: 'dark',
+    classSuffix: '',
+    storageKey: 'nuxt-color-mode',
+  },
+})
