6f2673f74d4961202c51548b43fc026d4f04b310
EliasSchaut <eschaut@outlook.de>
2023-11-30 20:26:27 +0100

    Update UI positioning to center everything on main page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="pb-3">
-    <div class="text-3xl font-bold text-gray-100 lg:text-5xl">
-      Xmas Task Calendar
+  <div class="flex flex-col justify-center align-middle">
+    <div class="flex flex-col justify-center pb-3 align-middle">
+      <div class="m-auto text-3xl font-bold text-gray-100 lg:text-5xl">
+        Xmas Task Calendar
+      </div>
+      <Waittime class="m-auto text-gray-200 lg:text-lg" />
     </div>
-    <Waittime class="text-gray-200 lg:text-lg" />
-  </div>
-  <div>
-    <ul class="grid max-w-5xl grid-cols-3">
-      <li v-for="i in range(1, 25)" :key="i" class="relative">
-        <Imgcard
-          :index="i"
-          :day="permutation(i)"
-          :onSelect="() => show(permutation(i))"
-        />
-      </li>
-    </ul>
+    <div class="m-auto">
+      <ul class="grid max-w-5xl grid-cols-3">
+        <li v-for="i in range(1, 25)" :key="i" class="relative">
+          <Imgcard
+            :index="i"
+            :day="permutation(i)"
+            :onSelect="() => show(permutation(i))"
+          />
+        </li>
+      </ul>
+    </div>
   </div>
   <Modal ref="modal" />
 </template>
